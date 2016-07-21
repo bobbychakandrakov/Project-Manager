7bd8e05c9538a1ce3c597ef724955ace6328fd7c
Petar Yanakiev <petar.yanakiev@sofia.stanga.net>
2016-07-21 14:02:47 +0300

    Login and Register validation

--- client/app/app.js
+++ client/app/app.js
@@ -26,4 +26,22 @@
   .constant('BASE_URL','http://localhost:7777/api')
   .constant('CONTENT_TYPE', 'application/x-www-form-urlencoded; charset=UTF-8');
 
+  toastr.options = {
+    closeButton: false,
+    debug: false,
+    newestOnTop: false,
+    progressBar: false,
+    positionClass: "toast-bottom-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut"
+  };
+
 })();
--- client/app/authentication/authentication.js
+++ client/app/authentication/authentication.js
@@ -1,63 +1,75 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('projectManager.authentication', [])
-    .controller('AuthenticationController', AuthenticationController);
+    angular
+        .module('projectManager.authentication', [])
+        .controller('AuthenticationController', AuthenticationController);
 
-  AuthenticationController.$inject = ['$scope','authenticationService', '$location','toastr'];
+    AuthenticationController.$inject = ['$scope', 'authenticationService', '$location', 'toastr', '$route'];
 
-  function AuthenticationController($scope, authenticationService, $location, toastr){
+    function AuthenticationController($scope, authenticationService, $location, toastr, $route) {
 
-    $scope.loginUser = {
-      password:'',
-      email:''
-    };
+        $scope.loginUser = {
+            password: '',
+            email: ''
+        };
 
-    $scope.registerUser = {
-      name:'',
-      password:'',
-      position:'',
-      email:''
-    };
+        $scope.registerUser = {
+            name: '',
+            password: '',
+            position: '',
+            email: ''
+        };
 
-    $scope.login = login;
+        $scope.login = login;
 
-    $scope.register = register;
+        $scope.register = register;
 
-    function login() {
-      authenticationService.login($scope.loginUser).then(function (data) {
-        toastr.success('Welcome!');
-        $location.path('/dashboard');
-      },function (err) {
-        console.log(err);
-      });
+        function login(LoginForm) {
+            if (LoginForm.$valid) {
+                authenticationService.login($scope.loginUser).then(function (data) {
+                    toastr.success('Welcome!');
+                    $location.path('/dashboard');
+                }, function (err) {
+                    console.log(err);
+                });
+            }
+            else {
+                toastr["error"]("Login Failed!");
+            }
+        }
+
+        function register(RegisterForm) {
+            if (RegisterForm.$valid) {
+                console.log($scope.registerUser);
+                authenticationService.register($scope.registerUser).then(function (data) {
+                    console.log(data);
+                    toastr["success"]("Success!");
+                    $route.reload();
+                }, function (err) {
+                    console.log(err);
+                });
+            }
+            else {
+                toastr["error"]("Register Failed!");
+            }
+        }
+
+        $('#login-form-link').click(function (e) {
+            $("#login-form").delay(100).fadeIn(100);
+            $("#register-form").fadeOut(100);
+            $('#register-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+        $('#register-form-link').click(function (e) {
+            $("#register-form").delay(100).fadeIn(100);
+            $("#login-form").fadeOut(100);
+            $('#login-form-link').removeClass('active');
+            $(this).addClass('active');
+            e.preventDefault();
+        });
+
     }
-
-    function register() {
-      console.log($scope.registerUser);
-      authenticationService.register($scope.registerUser).then(function (data) {
-        console.log(data);
-      }, function (err) {
-        console.log(err);
-      });
-    }
-
-    $('#login-form-link').click(function(e) {
-   		$("#login-form").delay(100).fadeIn(100);
-    		$("#register-form").fadeOut(100);
-   		$('#register-form-link').removeClass('active');
-   		$(this).addClass('active');
-   		e.preventDefault();
-   	});
-   	$('#register-form-link').click(function(e) {
-   		$("#register-form").delay(100).fadeIn(100);
-    		$("#login-form").fadeOut(100);
-   		$('#login-form-link').removeClass('active');
-   		$(this).addClass('active');
-   		e.preventDefault();
-   	});
-
-  }
 
 })();
